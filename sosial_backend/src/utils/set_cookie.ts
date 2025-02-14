295508293fb6746828889b4c83ae3131edb98d47
Pito Desri Pauzi <pitokfauzi@gmail.com>
2025-02-14 10:27:47 +0700

    dd

--- sosial_backend/src/utils/set_cookie.ts
+++ sosial_backend/src/utils/set_cookie.ts
@@ -4,10 +4,10 @@ export const setCookie = (name: string, value: any, maxAge: number, res: Respons
     res.cookie(name, value,
         {
             maxAge: maxAge,
-            partitioned: process.env.NODE_ENV === "production",
+            // partitioned: process.env.NODE_ENV === "production",
             httpOnly: true,
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-            secure: process.env.NODE_ENV === "production"
+            // sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            // secure: process.env.NODE_ENV === "production"
         }
     )
 }
