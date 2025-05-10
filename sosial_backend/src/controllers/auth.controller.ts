40bb34df97608213056404ac3d6bd4568ca221f6
Pito Desri Pauzi <pitokfauzi@gmail.com>
2025-05-10 23:19:48 +0700

    sad

--- sosial_backend/src/controllers/auth.controller.ts
+++ sosial_backend/src/controllers/auth.controller.ts
@@ -46,7 +46,7 @@ export const loginController = async (req: Request, res: Response<responseApi>) 
 
         const accessToken = await generateToken(payload);
         // res.cookie("accessToken", accessToken, { maxAge: (7 * 24 * 60 * 60 * 1000) })
-        setCookie('accessToken', accessToken, (7 * 24 * 60 * 60 * 1000), res)
+        // setCookie('accessToken', accessToken, (7 * 24 * 60 * 60 * 1000), res)
         return res.status(200).json({
             success: true,
             statusCode: 200,
